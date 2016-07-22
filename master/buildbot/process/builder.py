78e198d1956483eb552968494a362167fc58c7d4
Giuseppe Di Natale <dinatale2@users.noreply.github.com>
2016-07-22 13:44:55 -0700

    Resolved a NoneType exception during build cleanup. (#9)
    In Builder._startBuildFor, it is possible for slavebuilder.slave
    to be None for latent slaves, therefore check for None before
    releasing locks.
    
    Signed-off-by: Giuseppe Di Natale <dinatale2@llnl.gov>

--- master/buildbot/process/builder.py
+++ master/buildbot/process/builder.py
@@ -302,12 +302,12 @@ class Builder(config.ReconfigurableServiceMixin,
         cleanups = []
 
         def run_cleanups():
-            try:
-                while cleanups:
-                    fn = cleanups.pop()
+            while cleanups:
+                fn = cleanups.pop()
+                try:
                     fn()
-            except:
-                log.err(failure.Failure(), "while running %r" % (run_cleanups,))
+                except:
+                    log.err(failure.Failure(), "while running %r" % (run_cleanups,))
 
         # the last cleanup we want to perform is to update the big
         # status based on any other cleanup
@@ -319,7 +319,10 @@ class Builder(config.ReconfigurableServiceMixin,
 
         # set up locks
         build.setLocks(self.config.locks)
-        cleanups.append(lambda: slavebuilder.slave.releaseLocks())
+        def slaveLockCleanup():
+            if slavebuilder.slave is not None:
+                slavebuilder.slave.releaseLocks()
+        cleanups.append(slaveLockCleanup)
 
         if len(self.config.env) > 0:
             build.setSlaveEnvironment(self.config.env)
